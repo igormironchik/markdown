b600f64b79b191980bfa62b411fbc62554eac315
Igor Mironchik <igor.mironchik@gmail.com>
2023-04-03 14:08:13 +0300

    Fix clean.sh.

--- clean.cmd
+++ clean.cmd
@@ -1,7 +1,7 @@
 rmdir /S /Q Qt
 rmdir /S /Q build-md-editor
 rmdir /S /Q build-md-pdf
-del /Q Markdown_Linux_x64.Installer
+del /Q Markdown_Windows_x64.Installer.exe
 del /Q aqtinstall.log
 rmdir /S /Q installer\packages\mironchik.igor.markdown\data\bin
 rmdir /S /Q installer\packages\mironchik.igor.markdown\data\plugins
