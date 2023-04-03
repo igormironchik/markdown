731f471dda1f759171c204b0a90cfe399992a129
Igor Mironchik <igor.mironchik@gmail.com>
2023-04-03 13:42:36 +0300

    Clean clean.cmd.

--- clean.cmd
+++ clean.cmd
@@ -4,8 +4,6 @@ rmdir /S /Q build-md-pdf
 del /Q Markdown_Linux_x64.Installer
 del /Q aqtinstall.log
 rmdir /S /Q installer\packages\mironchik.igor.markdown\data\bin
-rmdir /S /Q installer\packages\mironchik.igor.markdown\data\lib
 rmdir /S /Q installer\packages\mironchik.igor.markdown\data\plugins
-rmdir /S /Q installer\packages\mironchik.igor.markdown\data\libexec
 rmdir /S/ Q installer\packages\mironchik.igor.markdown\data\resources
 rmdir /S /Q installer\packages\mironchik.igor.markdown\data\translations
